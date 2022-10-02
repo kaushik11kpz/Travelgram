22279860b2cb5f5b9ace99ae0e4283c443b670b8
sonam4012 <kaushikkpz888@gmail.com>
2022-10-02 12:53:12 +0530

    first release

---
+++ travelgram/src/app/pages/home/home.component.ts
@@ -0,0 +1,56 @@
+import { Component, OnInit } from '@angular/core';
+import { AngularFireDatabase } from '@angular/fire/compat/database';
+import { ToastrService } from 'ngx-toastr';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  users = [];
+  posts = [];
+
+  isLoading = false;
+
+  constructor(
+    private toastr : ToastrService,
+    private db: AngularFireDatabase
+  ) {
+    this.isLoading = true;
+    //get all users from firebase
+    db.object('/users')
+    .valueChanges()
+    .subscribe((obj) => {
+      if(obj){
+        this.users = Object.values(obj);
+        this.isLoading = false;
+      }
+      else{
+        toastr.error("No User Found");
+        this.users = [];
+        this.isLoading = false;
+      }
+    });
+    //get all posts from firebase
+    db.object('/posts')
+    .valueChanges()
+    .subscribe((obj) => {
+      if(obj){
+        this.posts = Object.values(obj).sort((a,b) => b.date -a.date);
+        this.isLoading = false;
+      }
+      else{
+        toastr.error("No Post to display");
+        this.posts = [];
+        this.isLoading = false;
+      }
+    });
+
+   }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ travelgram/src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyDH15JfCiMoEBwAsZl25Lgg84m_ZznCSOQ",
+    authDomain: "newone-e3d68.firebaseapp.com",
+    databaseURL: "https://newone-e3d68-default-rtdb.firebaseio.com",
+    projectId: "newone-e3d68",
+    storageBucket: "newone-e3d68.appspot.com",
+    messagingSenderId: "350994061291",
+    appId: "1:350994061291:web:240eba3487b273219aebae"
+  },
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
